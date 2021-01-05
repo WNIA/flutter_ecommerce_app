23146041ecde56b77dbca2aeaad62439cba11469
WNIA <wnia22@gmail.com>
2021-01-05 21:36:51 +0600

    ...

--- flutter_training/lib/movie_database/styles/text_styles.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const movieRowInfoItem = TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white);
-
-const movieTitleTxt = TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white);
