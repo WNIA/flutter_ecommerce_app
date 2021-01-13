3b8960b59309e065373961369d82140b4d2abc1c
WNIA <wnia22@gmail.com>
2021-01-13 23:02:31 +0600

    api fetch success, pagination/output troubles

--- lib/pages/pending_order_page.dart
+++ lib/pages/pending_order_page.dart
@@ -1,8 +1,4 @@
-
-import 'package:autism_project_demo_2/models/pending_order_request_model.dart';
-import 'package:autism_project_demo_2/models/pending_order_response_model.dart';
 import 'package:autism_project_demo_2/services/pending_order_api_service.dart';
-import 'package:autism_project_demo_2/widgets/pending_order_list.dart';
 import 'package:autism_project_demo_2/widgets/pending_order_list_pagination.dart';
 import 'package:flutter/material.dart';
 import 'package:autism_project_demo_2/services/pending_order_local_json.dart';
@@ -13,48 +9,14 @@ class PendingOrderPage extends StatefulWidget {
 }
 
 class _PendingOrderPageState extends State<PendingOrderPage> {
-  PendingOrderAPIService _pendingOrderAPIService = new PendingOrderAPIService();
-  PendingOrderRequestModel _requestModel;
   LocalJsonService local = new LocalJsonService();
+  PendingOrderAPIService _apiService = new PendingOrderAPIService();
 
   @override
   void initState() {
-    _requestModel = new PendingOrderRequestModel();
-    // getPendingOrderList();
     super.initState();
   }
 
-  /*
-  * to fetch pending order list from rest api
-  * UNUSED FOR NOW
-  * @WNIA
-  */
-  getPendingOrderList() async {
-    _requestModel.limit = 10;
-    _requestModel.page = 1;
-    _requestModel.latitude = 23.7747523;
-    _requestModel.longititude = 90.3654215;
-
-    _pendingOrderAPIService
-        .fetchPendingOrderResponse(_requestModel)
-        .then((value) {
-      if (value != null) {
-        print("...");
-      }
-    });
-  }
-
-  getPendingOrderFromJson(response) {
-    try {
-      if (response == null)
-        return null;
-      else
-        return response =
-            pendingOrderResponseFromJson(response.data.toString());
-    } catch (e) {
-      print(e);
-    }
-  }
 
   /*
    * FutureBuilder is fetching data from local json file
@@ -62,29 +24,12 @@ class _PendingOrderPageState extends State<PendingOrderPage> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: local.fetchLocalJson(context, 1),
-        builder: (context, snapshot){
+        future: _apiService.fetchPendingOrderPagination(1),
+        builder: (context, snapshot) {
           List orders = snapshot.data;
-      return snapshot.hasData ? PendingOrderListPagination(orders, 1) : Center(child: CircularProgressIndicator());
-    });
-
-  }
-//UNUSED FOR NOW - WNIA
-  FutureBuilder<String> buildFutureBuilder(BuildContext context) {
-    return FutureBuilder(
-    future: fetchData(context, 1),
-    builder: (context, snapshot) {
-      PendingOrderResponseModel order = getPendingOrderFromJson(snapshot);
-      return order != null
-          ? PendingOrderList(order, 1)
-          : Center(child: CircularProgressIndicator());
-    },
-  );
-  }
-//UNUSED WNIA
-  Future<String> fetchData(BuildContext context, int page) async {
-    String response = await DefaultAssetBundle.of(context)
-        .loadString('assets/json/pending_order_${page}.json');
-    return response;
+          return snapshot.hasData
+              ? PendingOrderListPagination(orders, 1)
+              : Center(child: CircularProgressIndicator());
+        });
   }
 }
